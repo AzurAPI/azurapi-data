edb327f59c100596145b4c2da2d68e72381bf74a
octo-kumo <h1710169@nushigh.edu.sg>
2020-09-20 14:46:46 +0800

    Ship names

--- index.js
+++ index.js
@@ -4,4 +4,9 @@ Entry point!
 const fs = require("fs");
 const ships = require("./ships");
 
-fs.writeFileSync("./dist/ships.json", JSON.stringify(ships.readFilesFromLanguage(), null, '\t'));
+ships.readFilesFromLanguage("EN");
+ships.readFilesFromLanguage("CN");
+ships.readFilesFromLanguage("JP");
+ships.readFilesFromLanguage("KR");
+ships.readFilesFromLanguage("TW");
+fs.writeFileSync("./dist/ships.json", JSON.stringify(ships.compiled, null, '\t'));
--- ships/index.js
+++ ships/index.js
@@ -1,30 +1,46 @@
 const rarity = {
-    "2": "Common",
-    "3": "Rare",
-    "4": "Elite",
-    "5": "Super Rare/Priority",
-    "6": "Decisive"
-}
+    2: "Common",
+    3: "Rare",
+    4: "Elite",
+    5: "Super Rare/Priority",
+    6: "Decisive"
+};
+const nationality = {
+    0: "Universal", 1: "Eagle Union", 2: "Royal Navy",
+    3: "Sakura Empire", 4: "Iron Blood", 5: "Dragon Empery",
+    6: "Sardegna Empire", 7: "Northern Parliament", 8: "Iris Libre",
+    9: "Vichya Dominion", 98: "Universal", 101: "Neptunia",
+    104: "Kizuna AI", 105: "Hololive"
+};
+
+let compiled = {};
 
 function readFilesFromLanguage(lang = "EN") {
-    let compiled = {};
     let groups = require("../AzurLaneSourceJSON/" + lang + "/sharecfg/ship_data_group.json");
     let ships = require("../AzurLaneSourceJSON/" + lang + "/sharecfg/ship_data_template.json");
     let stats = require("../AzurLaneSourceJSON/" + lang + "/sharecfg/ship_data_statistics.json");
     let types = require("../AzurLaneSourceJSON/" + lang + "/sharecfg/ship_data_by_type.json")
-
     for (let id of Object.keys(groups)) {
         if (id === "all") continue;
         let group = groups[id];
-        compiled[group.group_type] = {
+        let ship = compiled[group.group_type];
+        if (!ship) compiled[group.group_type] = ship = {
             id: group.group_type,
             code: group.code,
-            type: types[group.type].type_name,
+
+            name: null,
+            tags: null,
+
+            type: group.type,
+            type_text: {},
+            type_team: group.team_type,
             nationality: group.nationality,
+            nationality_text: nationality[group.nationality],
             property_hexagon: group.property_hexagon,
             // rarity: [],
-            data: []
+            data: {}
         };
+        ship.type_text[lang.toLowerCase()] = types[group.type].type_name;
     }
 
     for (let id of Object.keys(ships)) {
@@ -32,36 +48,29 @@ function readFilesFromLanguage(lang = "EN") {
         let ship = ships[id];
         let stat = stats[id];
         // compiled[ship.group_type].rarity.push(rarity[stat.rarity])
-        compiled[ship.group_type].data.push({
+        if (ship.oil_at_start === 0) continue; // pseudo ship
+        if (compiled[ship.group_type].nationality !== stat.nationality) continue; // pseudo ship
+        compiled[ship.group_type].stars = ship.star_max;
+        compiled[ship.group_type].name = ship.english_name;
+        if (compiled[ship.group_type].tags && JSON.stringify(compiled[ship.group_type].tags) !== JSON.stringify(stat.tag_list)) console.log("DIED: " + ship.id);
+        compiled[ship.group_type].tags = stat.tag_list;
+
+        let specificShip = compiled[ship.group_type].data[ship.id];
+        if (!specificShip) compiled[ship.group_type].data[ship.id] = specificShip = {
             id: ship.id,
             tags: stat.tag_list,
-            type: types[ship.type].type_name,
-            energy: ship.energy,
-            names: {
-                code: stat.english_name,
-                name: stat.name
-            },
+            type: {},
+            names: {},
             rarity: stat.rarity,
             rarity_text: rarity[stat.rarity],
-            nationality: stat.nationality,
-            stars: {
-                current: ship.star,
-                max: ship.star_max
-            },
-            oil: {
-                base: ship.oil_at_start,
-                max: ship.oil_at_end
-            },
-            level: {
-                max: ship.max_level
-            }
-        });
-    }
-    for (let id of Object.keys(compiled)) {
-        let ship = compiled[id];
-        ship.data.sort((a, b) => a.rarity - b.rarity);
+            stars: ship.star,
+            oil: ship.oil_at_end,
+            max_level: ship.max_level
+        };
+        specificShip.type[lang.toLowerCase()] = types[ship.type].type_name;
+        specificShip.names[lang.toLowerCase()] = stat.name;
     }
     return compiled;
 }
 
-module.exports = {readFilesFromLanguage};
+module.exports = {readFilesFromLanguage, compiled};
