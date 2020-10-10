cd47dbfe5531bee1edee0c88ead176da3f904080
octo-kumo <h1710169@nushigh.edu.sg>
2020-10-10 12:11:01 +0800

    Makes data cleaner

--- ships/index.js
+++ ships/index.js
@@ -1,11 +1,11 @@
-const rarity = {
+const RARITY = {
     2: "Common",
     3: "Rare",
     4: "Elite",
     5: "Super Rare/Priority",
     6: "Decisive"
 };
-const nationality = {
+const NATIONALITY = {
     0: "Universal", 1: "Eagle Union", 2: "Royal Navy",
     3: "Sakura Empire", 4: "Iron Blood", 5: "Dragon Empery",
     6: "Sardegna Empire", 7: "Northern Parliament", 8: "Iris Libre",
@@ -28,14 +28,12 @@ function readFilesFromLanguage(lang = "EN") {
             id: group.group_type,
             code: group.code,
 
-            name: null,
-            tags: null,
+            name: {},
 
             type: group.type,
             type_text: {},
-            type_team: group.team_type,
             nationality: group.nationality,
-            nationality_text: nationality[group.nationality],
+
             property_hexagon: group.property_hexagon,
             // rarity: [],
             data: {}
@@ -47,13 +45,17 @@ function readFilesFromLanguage(lang = "EN") {
         if (id === "all") continue;
         let ship = ships[id];
         let stat = stats[id];
+
+        if (!ship || !stat) continue; // ship not here / not complete
+
         // compiled[ship.group_type].rarity.push(rarity[stat.rarity])
         if (ship.oil_at_start === 0) continue; // pseudo ship
         if (compiled[ship.group_type].nationality !== stat.nationality) continue; // pseudo ship
         compiled[ship.group_type].stars = ship.star_max;
-        compiled[ship.group_type].name = ship.english_name;
-        if (compiled[ship.group_type].tags && JSON.stringify(compiled[ship.group_type].tags) !== JSON.stringify(stat.tag_list)) console.log("DIED: " + ship.id);
-        compiled[ship.group_type].tags = stat.tag_list;
+
+        if (compiled[ship.group_type].name.code && compiled[ship.group_type].name.code !== ship.english_name) console.log("SHIP CODE NAME NOT MATCH ", id, ship.group_type, stat.name, lang)
+
+        compiled[ship.group_type].name.code = ship.english_name;
 
         // https://github.com/minhducsun2002/boomer/blob/92c21b3624b539068ef3758d7f4c879fc8401952/src/db/al/models/ship_data_statistics.ts
         let [hp, fp, trp, aa, av, rld, _, acc, eva, spd, luk, asw] = stat.attrs;
@@ -61,20 +63,20 @@ function readFilesFromLanguage(lang = "EN") {
         let specificShip = compiled[ship.group_type].data[ship.id];
         if (!specificShip) compiled[ship.group_type].data[ship.id] = specificShip = {
             id: ship.id,
-            tags: stat.tag_list,
-            type: {},
-            names: {},
+            tags: stat.tag_list.length > 0 ? stat.tag_list : undefined, // save space
+            type: ship.type,
             rarity: stat.rarity,
-            rarity_text: rarity[stat.rarity],
             stars: ship.star,
             oil: ship.oil_at_end,
             max_level: ship.max_level,
-            stats: {
-                hp, fp, trp, aa, av, rld, acc, eva, spd, luk, asw
-            }
+            stats: {hp, fp, trp, aa, av, rld, acc, eva, spd, luk, asw}
         };
-        specificShip.type[lang.toLowerCase()] = types[ship.type].type_name;
-        specificShip.names[lang.toLowerCase()] = stat.name;
+        if (specificShip.type !== ship.type) console.log("SHIP TYPE NOT MATCH ", id, ship.group_type, stat.name, lang)
+        if (compiled[ship.group_type].name[lang.toLowerCase()] && compiled[ship.group_type].name[lang.toLowerCase()] !== stat.name) { // name not matching, probably retrofit
+            if (!specificShip.name) specificShip.name = {};
+            specificShip.name[lang.toLowerCase()] = stat.name;
+        }
+        compiled[ship.group_type].name[lang.toLowerCase()] = stat.name;
     }
     return compiled;
 }
