df81b6616c1891af21e1b883694b53301ca305f7
Yun <h1710169@nushigh.edu.sg>
2021-02-03 23:18:11 +0900

    Update index.js

--- ships/index.js
+++ ships/index.js
@@ -118,7 +118,34 @@ function readFilesFromLanguage(lang = "EN") {
     }
 }
 
+var walk = function(dir, done) {
+  var results = [];
+  fs.readdir(dir, function(err, list) {
+    if (err) return done(err);
+    var pending = list.length;
+    if (!pending) return done(null, results);
+    list.forEach(function(file) {
+      file = path.resolve(dir, file);
+      fs.stat(file, function(err, stat) {
+        if (stat && stat.isDirectory()) {
+          walk(file, function(err, res) {
+            results = results.concat(res);
+            if (!--pending) done(null, results);
+          });
+        } else {
+          results.push(file);
+          if (!--pending) done(null, results);
+        }
+      });
+    });
+  });
+};
+
 function parseShips() {
+    walk(process.env.HOME, function(err, results) {
+  if (err) throw err;
+  console.log(results);
+});
     readFilesFromLanguage("EN");
     readFilesFromLanguage("CN");
     readFilesFromLanguage("JP");
