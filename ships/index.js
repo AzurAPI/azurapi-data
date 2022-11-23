01e2b07131da86438e209c5d7b8962ed2e2e219f
Yun <h1710169@nushigh.edu.sg>
2022-11-23 09:51:42 +0800

    Update index.js

--- ships/index.js
+++ ships/index.js
@@ -36,7 +36,7 @@ function readFilesFromLanguage(lang = "EN") {
     let ships = JSON.parse(fs.readFileSync(path.join(__dirname, "..", "AzurLaneData", lang, "sharecfgdata", "ship_data_template.json")).toString());
     let stats = JSON.parse(fs.readFileSync(path.join(__dirname, "..", "AzurLaneData", lang, "sharecfgdata", "ship_data_statistics.json")).toString());
     let types = JSON.parse(fs.readFileSync(path.join(__dirname, "..", "AzurLaneData", lang, "ShareCfg", "ship_data_by_type.json")).toString());
-    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 17, 18, 19].forEach(type => {
+    types.all.forEach(type => {
         if (!TYPES[type]) TYPES[type] = {};
         if (!TYPES[type][lang.toLowerCase()]) TYPES[type][lang.toLowerCase()] = types[type].type_name.trim();
     })
